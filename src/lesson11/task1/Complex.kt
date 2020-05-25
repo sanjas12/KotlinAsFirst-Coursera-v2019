674a8358dc318c9e9a2220f9e56e532648c34e37
sanja_s <sanja100v@mail.ru>
2020-05-26 00:09:01 +0300

    comment future unsolved examles

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -11,50 +11,50 @@ package lesson11.task1
  *
  * Аргументы конструктора -- вещественная и мнимая часть числа.
  */
-class Complex(val re: Double, val im: Double) {
-
-    /**
-     * Конструктор из вещественного числа
-     */
-    constructor(x: Double) : this(TODO(), TODO())
-
-    /**
-     * Конструктор из строки вида x+yi
-     */
-    constructor(s: String) : this(TODO(), TODO())
-
-    /**
-     * Сложение.
-     */
-    operator fun plus(other: Complex): Complex = TODO()
-
-    /**
-     * Смена знака (у обеих частей числа)
-     */
-    operator fun unaryMinus(): Complex = TODO()
-
-    /**
-     * Вычитание
-     */
-    operator fun minus(other: Complex): Complex = TODO()
-
-    /**
-     * Умножение
-     */
-    operator fun times(other: Complex): Complex = TODO()
-
-    /**
-     * Деление
-     */
-    operator fun div(other: Complex): Complex = TODO()
-
-    /**
-     * Сравнение на равенство
-     */
-    override fun equals(other: Any?): Boolean = TODO()
-
-    /**
-     * Преобразование в строку
-     */
-    override fun toString(): String = TODO()
-}
+//class Complex(val re: Double, val im: Double) {
+//
+//    /**
+//     * Конструктор из вещественного числа
+//     */
+////    constructor(x: Double) : this(TODO(), TODO())
+//
+//    /**
+//     * Конструктор из строки вида x+yi
+//     */
+////    constructor(s: String) : this(TODO(), TODO())
+//
+//    /**
+//     * Сложение.
+//     */
+//    operator fun plus(other: Complex): Complex = TODO()
+//
+//    /**
+//     * Смена знака (у обеих частей числа)
+//     */
+//    operator fun unaryMinus(): Complex = TODO()
+//
+//    /**
+//     * Вычитание
+//     */
+//    operator fun minus(other: Complex): Complex = TODO()
+//
+//    /**
+//     * Умножение
+//     */
+//    operator fun times(other: Complex): Complex = TODO()
+//
+//    /**
+//     * Деление
+//     */
+//    operator fun div(other: Complex): Complex = TODO()
+//
+//    /**
+//     * Сравнение на равенство
+//     */
+//    override fun equals(other: Any?): Boolean = TODO()
+//
+//    /**
+//     * Преобразование в строку
+//     */
+//    override fun toString(): String = TODO()
+//}
